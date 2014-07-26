a6d080d0eeb1a71beccd3ff77c4dda1e105409b1
David Manescu <david.manescu@gmail.com>
2014-07-26 23:21:55 +1000

    wrote build-trees

---
+++ ai/map-functions.lisp
@@ -0,0 +1,119 @@
+(include "consts.lisp")
+(include "functions.lisp")
+
+(defun dbug-trees () (dbug
+  (let ((x 2) (y 0))
+    (let ((first-row (cons 1 (cons 1 (cons 0 0))))
+          (second-row (cons 0 (cons 1 (cons 1 0))))
+          (third-row (cons 0 (cons 0 (cons 1 0))))
+         )
+      (let ((trees (build-trees (cons first-row (cons second-row (cons third-row 0))))))
+        (dbug (2d-nth trees x y))
+      )
+    )
+  )
+))
+
+(defun build-trees (world)
+  (let ((rest_of_world world))
+    (mapi
+      (lambda (row y)
+        (set rest_of_world (cdr rest_of_world))
+        (build-trees-row
+          0 y row ; initial_x y initial_rest_of_row
+            (if y (list-nth world (- y 1)) 0) ; prev_row if available
+            row ; whole_row
+            rest_of_world
+            world
+        )
+      )
+      world
+    )
+  )
+)
+
+(defun build-trees-row (x y rest_of_row  prev_row whole_row rest_of_world world)
+  (if (atom rest_of_row) 0
+    (cons
+      (car (visit-cell
+            (cons x y) ; xy
+            rest_of_row ; rest_of_row
+            prev_row ; prev_row
+            whole_row ; whole_row
+            rest_of_world ; rest_of_world
+            world ; world
+            0 ; seen
+      ))
+      (build-trees-row (+ x 1) y (cdr rest_of_row) prev_row whole_row rest_of_world world)
+    )
+  )
+)
+
+
+(defun visit-cell (xy rest_of_row prev_row whole_row rest_of_world world seen)
+  (if (if (car rest_of_row) (not (point-not-in-list xy seen)) 1) ; exit via the true branch
+    (cons 0 seen) ; return immediately
+
+    (let ((children 0) (seen (cons xy seen)) (x (car xy)) (y (cdr xy)))
+      ; (dbug xy)
+      ; (dbug seen)
+  
+      ; right-move (cheapest)
+      (if (atom (cdr rest_of_row)) ; end of row?
+        (set children (cons 0 children)) ; don't try to move right or add tree-right to the children list
+        (let ((result (visit-cell (cons (+ x 1) y) (cdr rest_of_row)
+                                  prev_row whole_row rest_of_world world seen)))
+          (set children (cons (car result) children))
+          (set seen (cdr result))
+        )
+      )
+
+      ;(if (not (+ x y)) (dbug children) (pass))
+      ;(if (and (= x 2) (= y 1)) (do (dbug rest_of_world) (brk)) (pass))
+      ;(dbug xy)
+      ; down-move
+      (if (atom rest_of_world)
+        (set children (cons 0 children)) ; can't move down
+        (let ((result (visit-cell (cons x (+ y 1)) (nth-cell (car rest_of_world) x)
+                       whole_row (car rest_of_world) (cdr rest_of_world) world seen)))
+          (set children (cons (car result) children))
+          (set seen (cdr result))
+        )
+      )
+
+      ;(if (not (+ x y)) (dbug children) (pass))
+
+      ; left-move
+      (if x
+        (let ((result (visit-cell (cons (- x 1) y)
+                      (cons (list-nth whole_row (- x 1)) rest_of_row) ; push left-element on
+                      prev_row whole_row rest_of_world world seen)))
+          (set children (cons (car result) children))
+          (set seen (cdr result))
+        )
+        (set children (cons 0 children)) ; couldn't move left
+      )
+
+      ;(if (not (+ x y)) (dbug children) (pass))
+
+      ; up-move
+      (if y
+        (let ((result (visit-cell (cons x (- y 1)) (nth-cell prev_row x)
+                                  (if (= y 1) 0 (list-nth world (- y 2))) ; prev-prev-row
+                                  prev_row (cons whole_row rest_of_world) world seen)))
+          (set children (cons (car result) children))
+          (set seen (cdr result))
+        )
+        (set children (cons 0 children)) ; couldn't move up
+      )
+
+      ;(if (not (+ x y)) (dbug children) (pass))
+
+      (set children (cons (car rest_of_row) children)) ; add my cell on
+
+      ;(if (not (+ x y)) (dbug children) (pass))
+
+      (cons children seen) ; return the created tuple and new seen list
+    )
+  )
+)
