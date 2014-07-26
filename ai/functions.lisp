b1af0351534ffcc4298b53568b8304917fbaffc6
Huon Wilson <dbau.pp+github@gmail.com>
2014-07-26 17:00:13 +1000

    General list helper functions.

--- ai/functions.lisp
+++ ai/functions.lisp
@@ -14,6 +14,48 @@
     ; a tuple index is the same as an list index except for the last one
    (list-nth tup n)))
 
+(defun foldl (f z list)
+  (if list
+      z
+    (foldl f (f z (car list)) (cdr list))))
+(defun foldr (f z list)
+  (if list
+      z
+    (f (car list) (foldr f z (cdr list)))))
+
+; create a new list by applying f to each element of list
+(defun map (f list)
+  (if
+      list
+      ((f (car list)) (map f (cdr list)))
+    0))
+
+; find the first element of list for which f is true returning (index,
+; (element, f element)), or 0 if it is not found.
+(defun find (f list)
+  (__find f list 0))
+
+(defun __find (f list i)
+  (if list
+      (let ((head (car list)))
+        (let ((result (f head)))
+          (if result
+              (cons i (cons head result))
+            (__find f (cdr list) (add i 1)))))
+    0))
+
+; create a new list with elem on the back
+(defun push-back (list elem)
+  (if list
+      (cons (car list) (push-back (cdr list) elem))
+    (cons elem 0)))
+
+; append the two lists a and b
+(defun append (a b)
+  (if a
+      (cons (car a) (append (cdr a) b))
+    b))
+
 ; booleans r fun
 (defun or (x y) (if x x y))
 (defun and (x y) (if x y 0))
