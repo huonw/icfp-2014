e92a5efa3b83f6fdb71a903aa4775a3558725cc7
Huon Wilson <dbau.pp+github@gmail.com>
2014-07-26 14:52:47 +1000

    Take input as files (concatenated) and some options.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,62 @@
 #![feature(macro_rules, phase)]
 
+extern crate getopts;
+
 extern crate regex;
 #[phase(plugin)] extern crate regex_macros;
 
-use std::io;
+use std::{os, io, result};
 
 mod asm;
 mod lisp;
 
+
 fn main() {
-    let string = io::stdin().read_to_string().unwrap();
-    //let x = asm::parse(string.as_slice());
-    //println!("{}", asm::compile(x.as_slice()));
-    let code = lisp::parse(string.as_slice());
-    //println!("parsed: {}", code);
-    let compiled = lisp::compile(&code);
-    //println!("labelled: {}", asm::print_labelled(compiled.as_slice()));
-    let asm = asm::compile(compiled.as_slice());
-    print!("{}", asm::print(asm.as_slice()));
+    let opts = [getopts::optopt("e", "emit", "what format to print", "bin|asm|ast"),
+                getopts::optflag("h", "help", "show this help message")];
+    let args = os::args();
+
+    let mut matches = getopts::getopts(args.tail(), opts).unwrap();
+    if matches.opt_present("help") {
+        println!("{}", getopts::usage(args[0].as_slice(), opts));
+        return;
+    }
+
+    if matches.free.is_empty() {
+        matches.free.push("-".to_string());
+    }
+
+    let input: io::IoResult<Vec<String>> = result::collect(
+        matches.free.iter().map(|name| {
+            if name.as_slice() == "-" {
+                io::stdin().read_to_string()
+            } else {
+                io::File::open(&Path::new(name.as_slice())).and_then(|mut r| r.read_to_string())
+            }
+        }));
+
+    let input = match input {
+        Ok(i) => i.concat(),
+        Err(e) => {
+            println!("error loading files: {}", e);
+            os::set_exit_status(1);
+            return
+        }
+    };
+
+    let code = lisp::parse(input.as_slice());
+
+    match matches.opt_str("emit").unwrap_or(String::new()).as_slice() {
+        "" | "bin" => {
+            print!("{}",
+                   asm::print(asm::compile(lisp::compile(&code).as_slice()).as_slice()));
+        }
+        "asm" => {
+            print!("{}",
+                   asm::print_labelled(lisp::compile(&code).as_slice()))
+
+        }
+        "ast" => println!("{}", code),
+        _ => fail!("unrecognised `emit`")
+    }
 }
